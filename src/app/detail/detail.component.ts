0003692849eb5601babe66badbccbbb85087d99d
ChisKien <chiskien03214@gmail.com>
2021-10-20 11:51:33 +0700

    injecting product service

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
+  public product: Product;
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
