4caa6becea6bc45440c301ff682d0f48cea4908e
ChisKien <chiskien03214@gmail.com>
2021-10-20 11:48:34 +0700

    Implementing Model

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,3 +1,11 @@
-export class Product {
-
+export interface Product {
+  id: number;
+  date: Date;
+  region_name: string;
+  area: string;
+  average_price: number;
+  index: number;
+  sale_volumn: number;
+  detached_price: number;
+  detached_index: number;
 }
