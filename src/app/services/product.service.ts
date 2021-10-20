762e326315e06694c30c0e5f53b048a1036dc5c3
ChisKien <chiskien03214@gmail.com>
2021-10-20 11:49:19 +0700

    Create an Obserable Product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProduct(): Observable<Product[]> {
+    return this.http.get<Product[]>('/api/hpiAdmins');
+  }
 }
