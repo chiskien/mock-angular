e471af2e35a435b55b647707a431d429103f8de2
ChisKien <chiskien03214@gmail.com>
2021-10-21 19:42:03 +0700

    finish pass data through edit page

--- src/app/edit-page/edit-page.component.ts
+++ src/app/edit-page/edit-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {ActivatedRoute} from "@angular/router";
+import {Product} from "../models/product";
 
 @Component({
   selector: 'app-edit-page',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-page.component.scss']
 })
 export class EditPageComponent implements OnInit {
+  index: string[] = ["Date", "RegionName", "Area", "AveragePrice", "Index",
+    "SalesVolume", "DetachedPrice", "DetachedIndex"];
+  public _product: Product;
 
-  constructor() { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    let id = Number.parseInt(this.route.snapshot.params["id"]);
+    this.productService.getProductbyId(id).subscribe((response) => {
+      this._product = response;
+    })
   }
 
 }
