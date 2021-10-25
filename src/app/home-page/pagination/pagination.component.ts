665f693f311ef1b17fdd2cb78b50713bf1554419
ChisKien <chiskien03214@gmail.com>
2021-10-25 10:06:12 +0700

    I don't know what i'm doing right now

--- src/app/home-page/pagination/pagination.component.ts
+++ src/app/home-page/pagination/pagination.component.ts
@@ -9,11 +9,18 @@ export class PaginationComponent implements OnInit {
   title: string = "Pagination works";
   @Input() numberOfPages: number = 0;
   options: number[] = [10, 20, 50, 100];
+  
+  paginate = ({rows, entries, activePage}) => {
+    const firstVisibleEntry = activePage * entries;
+    return rows.slice(firstVisibleEntry, firstVisibleEntry + entries);
+  };
 
   constructor() {
   }
 
+
   ngOnInit(): void {
   }
 
+  /* eslint-disable indent */
 }
