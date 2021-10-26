d8b937daa9e82f143cb4873b692415021a20fb79
ChisKien <chiskien03214@gmail.com>
2021-10-26 19:10:45 +0700

    Update edit page

--- src/app/edit-page/edit-page.component.ts
+++ src/app/edit-page/edit-page.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from "../services/product.service";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {Product} from "../models/product";
 import {Location} from "@angular/common";
 import {map, switchMap} from "rxjs/operators";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   public _product: Product;
   Id: number;
   title: string = "Edit Product";
+  param: Subscription;
   message: string = '';
   regionNameOptions: string[] = ["Greater Manchester", "Merseyside", "South Yorkshire",
     "Tyne and Wear", "West Midlands", "West Yorkshire"];
@@ -30,7 +32,6 @@ export class EditPageComponent implements OnInit {
   ngOnInit(): void {
     this.getProduct();
     console.table(this.formGroup);
-
   }
 
   createForm(_product: Product): void {
@@ -42,6 +43,9 @@ export class EditPageComponent implements OnInit {
       areaCode: [_product?.AreaCode, Validators.required],
       averagePrice: [_product?.AveragePrice, [Validators.required]],
       index: [_product?.Index, [Validators.required]],
+      indexSA: [_product?.IndexSA],
+      averagePriceSA: [_product?.AveragePriceSA],
+      salesVolume: [_product?.SalesVolume],
       detachedPrice: [_product?.DetachedPrice, [Validators.required]],
       detachedIndex: [_product?.DetachedIndex, [Validators.required]],
       semiDetachedIndex: [_product?.SemiDetachedIndex, [Validators.required]],
@@ -50,11 +54,21 @@ export class EditPageComponent implements OnInit {
       terracedIndex: [_product?.TerracedIndex, [Validators.required]],
       flatIndex: [_product?.FlatIndex, [Validators.required]],
       flatPrice: [_product?.FlatPrice, [Validators.required]],
+      "1m%Change": [_product?.["1m%Change"]],
+      "12m%Change": [_product?.["12m%Change"]],
+      "Detached1m%Change": [_product?.["Detached1m%Change"]],
+      "Detached12m%Change": [_product?.["Detached12m%Change"]],
+      "SemiDetached12m%Change": [_product?.["SemiDetached12m%Change"]],
+      "SemiDetached1m%Change": [_product?.["SemiDetached1m%Change"]],
+      "Terraced1m%Change": [_product?.["Terraced1m%Change"]],
+      "Terraced12m%Change": [_product?.["Terraced12m%Change"]],
+      "Flat12m%Change": [_product?.["Flat12m%Change"]],
+      "Flat1m%Change": [_product?.["Flat1m%Change"]],
     })
   }
 
   getProduct(): void {
-    this.route.paramMap.pipe(
+    this.param = this.route.paramMap.pipe(
       switchMap((param: ParamMap) => {
         this.Id = +param.get("id");
         return this.productService.getProductbyId(this.Id);
@@ -64,47 +78,63 @@ export class EditPageComponent implements OnInit {
     });
   }
 
-  onChange(regionName: string) {
+  onChange(regionName) {
     switch (regionName) {
       case "Greater Manchester":
-        this._product.RegionName = regionName;
-        this._product.Area = "Northwest";
-        this._product.AreaCode = "E11000001";
+        this.formGroup.patchValue({
+          regionName: regionName,
+          area: "Northwest",
+          areaCode: "E11000001"
+        });
         break;
       case "Merseyside":
-        this._product.RegionName = regionName;
-        this._product.Area = "Northwest";
-        this._product.AreaCode = "E11000002";
+        this.formGroup.patchValue({
+          regionName: regionName,
+          area: "Northwest",
+          areaCode: "E11000002"
+        });
         break;
       case "South Yorkshire":
-        this._product.RegionName = regionName;
-        this._product.Area = "North";
-        this._product.AreaCode = "E11000003";
+        this.formGroup.patchValue({
+          regionName: regionName,
+          area: "North",
+          areaCode: "E11000003"
+        });
         break;
       case "Tyne and Wear":
-        this._product.RegionName = regionName;
-        this._product.Area = "North";
-        this._product.AreaCode = "E11000007";
+        this.formGroup.patchValue({
+          regionName: regionName,
+          area: "North",
+          areaCode: "E11000007"
+        });
         break;
       case "West Midlands":
-        this._product.RegionName = regionName;
-        this._product.Area = "Midlands";
-        this._product.AreaCode = "E11000005";
+        this.formGroup.patchValue({
+          regionName: regionName,
+          area: "Midlands",
+          areaCode: "E11000005"
+        });
         break;
       case "West Yorkshire":
-        this._product.RegionName = regionName;
-        this._product.Area = "North";
-        this._product.AreaCode = "E11000006";
+        this.formGroup.patchValue({
+          regionName: regionName,
+          area: "North",
+          areaCode: "E11000006"
+        });
         break;
-
     }
+
   }
 
-  save(product: Product): void {
-    if (this._product) {
-      this.productService.updateProduct(product)
-        .subscribe(() => this.goBack())
-    }
+  save(): void {
+    this._product = Object.assign(this._product, this.formGroup.value);
+    console.table(this.formGroup.value);
+    // if (this._product) {
+    //   this.productService.updateProduct(this.formGroup.value)
+    //     .subscribe(() => console.log(`Product with id: ${this.formGroup.value.id}
+    //     updated successfully`)
+    //     );
+    // }
   }
 
   reset() {
@@ -114,6 +144,9 @@ export class EditPageComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  ngOnDestroy() {
   }
 
   get id() {
@@ -174,5 +207,9 @@ export class EditPageComponent implements OnInit {
 
   get terracedIndex() {
     return this.formGroup.get("semiDetachedIndex");
+  }
+
+  get salesVolume() {
+    return this.formGroup.get("salesVolume");
   }
 }
